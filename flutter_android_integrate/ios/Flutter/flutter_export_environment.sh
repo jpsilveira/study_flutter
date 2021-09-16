32a461e7e6fbacf53298b4dbb4c40b47c1c4aecc
jpsilveira <bitsoft.jpsilveira@gmail.com>
2021-09-16 16:35:42 -0300

    flutter_android_integrate

---
+++ flutter_android_integrate/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Desenv\Flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Desenv\Fontes\study_flutter_old\flutter_view"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
